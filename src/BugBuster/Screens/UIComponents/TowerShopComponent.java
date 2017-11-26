291856ef510b47d2290cb7adbc693bbda9b96872
Delphboy <hps1945@live.co.uk>
2017-11-26 21:18:10 +0000

    Played around with the tower shop

--- src/BugBuster/Screens/UIComponents/TowerShopComponent.java
+++ src/BugBuster/Screens/UIComponents/TowerShopComponent.java
@@ -1,70 +1,19 @@
 package BugBuster.Screens.UIComponents;
 
 import BugBuster.GameObjects.Towers.Tower;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
+import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.image.Image;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 
-public class TowerShopComponent extends ScrollPane
+
+import java.beans.EventHandler;
+
+public class TowerShopComponent extends Pane
 {
-	private int numOfShopItems = 0;
-
 	public TowerShopComponent()
 	{
-		Pane paneToAdd = null;
-
-		// set scroll properties
-		setHbarPolicy(ScrollBarPolicy.NEVER);
-		setVbarPolicy(ScrollBarPolicy.AS_NEEDED);
-
-		// generate tower items
-		for (int i = 0; i < 5; i++)
-		{
-			paneToAdd = createPane(new Tower("Generic Tower", i, i));
-			if (paneToAdd != null)
-			{
-				paneToAdd.setLayoutX(0);
-				paneToAdd.setLayoutY(0 * numOfShopItems);
-				getChildren().add(paneToAdd);
-				numOfShopItems++;
-			}
-		}
-	}
-
-	private HBox createPane(Tower towerForPane)
-	{
-		HBox towerItem = new HBox();
-
-		towerItem.setMaxWidth(250);
-		towerItem.setMaxHeight(100);
-		towerItem.setMinWidth(250);
-		towerItem.setMinHeight(100);
-
-		Canvas canvas = new Canvas(250, 100);
-		GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
-
-		Label towerInfo = new Label(towerForPane.toString());
-		towerInfo.setLayoutX(10);
-		towerInfo.setLayoutY(10);
-
-		graphicsContext.drawImage(towerForPane.getImage(), 20, 20, 20, 20);
-		graphicsContext.setFill(Color.GREENYELLOW);
-		graphicsContext.fillRect(0,0,250,100);
-
-		Button btnBuyTower = new Button("Buy");
-
-		// Add information to shop item
-		towerItem.getChildren().add(canvas);
-		towerItem.getChildren().add(towerInfo);
-		towerItem.getChildren().add(btnBuyTower);
-
-		return towerItem;
+		Label lbl = new Label("I should provide a way of buying towers");
+		getChildren().add(lbl);
 	}
 }
